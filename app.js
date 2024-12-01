6f4269f789ed50b891c4f645a367fe707f54bb85
Rubin Sabu <rubinsabu@gmail.com>
2024-12-02 00:58:58 +0530

    Single sign-On with Google account

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();  //server instance
 const path = require("path");
-const env = require("dotenv").config();
+const dotenv = require("dotenv").config();
 const session = require("express-session");
+const passport= require("./config/passport")
 const db = require("./config/db");
 const userRouter = require("./routes/userRouter");
 db();
@@ -19,6 +20,14 @@ app.use(session({
         maxAge:72*60*60*1000
     }
 }))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req,res,next)=>{
+    res.set('cache-control','no-store');
+    next();
+});
 
 app.set("view engine","ejs");
 app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
