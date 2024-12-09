da1feb5b62a3109bd84fa9dd5c0d262ff0ab2fcb
Rubin Sabu <rubinsabu@gmail.com>
2024-12-10 01:10:10 +0530

    customer management, search, block, unblock user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -20,20 +20,23 @@ const userAuth = (req,res,next)=>{
 }
 
 const adminAuth = (req,res,next)=>{
-    User.findOne({isAdmin:true})
-    .then(data=>{
-        if(data){
-            next();
-        }else{
-            res.redirect("/admin/login")
-        }
-    })
-    .catch(error=>{
-        console.log("Error in adminauth middleware",error);
-        res.status(500).send("Internal Server Error");
-    })
+    if(req.session.admin){
+        User.findOne({isAdmin:true})
+        .then(data=>{
+            if(data){
+                next();
+            }else{
+                res.redirect("/admin/login")
+            }
+        })
+        .catch(error=>{
+            console.log("Error in adminauth middleware",error);
+            res.status(500).send("Internal Server Error");
+        })
+}else{
+    res.redirect('/admin/login');
 }
-
+};
 module.exports={
     userAuth,
     adminAuth
