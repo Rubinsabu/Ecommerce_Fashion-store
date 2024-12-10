609cd33e8468d654085414ed974e56df306af5a7
Rubin Sabu <rubinsabu@gmail.com>
2024-12-11 01:50:24 +0530

    edit category added

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -18,5 +18,7 @@ router.get('/category',adminAuth,categoryController.categoryInfo);
 router.post('/addCategory',adminAuth,categoryController.addCategory);
 router.get('/listCategory',adminAuth,categoryController.getListCategory);
 router.get('/unlistCategory',adminAuth,categoryController.getUnlistCategory);
+router.get('/editCategory',adminAuth,categoryController.getEditCategory);
+router.post('/editCategory/:id',adminAuth,categoryController.editCategory);
 
 module.exports = router;
