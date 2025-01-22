70a9a0656f49b0ebaf7c670143e4fcd0e9277813
Rubin Sabu <rubinsabu@gmail.com>
2025-01-22 21:55:53 +0530

    port corrected

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname,"public")));
 app.use("/admin",adminRouter);
 app.use("/",userRouter);
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT,'0.0.0.0',()=>{
     console.log("Server running..");
     
 })
